863e0662a102e520e5d35f9478f5ee6dffe0eed8
cxrdonx <jose.eduardo.cardona@gmail.com>
2021-08-14 17:09:18 -0600

    re

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 
-console.log("RUNNING");
 
 const API_URL = "http://localhost:8090/notepad-api/notes";
 //fetch petition
-const HTMLResponse = document.querySelector(".container");
+const HTMLResponse = document.querySelector(".notes--container");
 const div = document.createElement('div');
+div.className = "notes--container-dashboard";
 const lista = document.querySelector("lista-lista");
 
 fetch(API_URL+"/all")
@@ -36,5 +36,59 @@ fetch(API_URL+"/all")
  });
 
 
+
+
+ var window = document.getElementById("window-form")
+   window.style.display ="none";
+  
+ const agregar = document.getElementById("agregar");
+ const close_button = document.getElementById('close-button');
+
+
+agregar.addEventListener("click", function(){
+
+  
+  
+   if(window.style.display == "none"){
+     window.style.display = "block";
+
+   }else{
+      window.style.display = "none";
+   }
+});
+
+const guardar = document.getElementById("guardar");
+guardar.addEventListener("click", function(){
+       const title = new FormData(document.getElementById('formulario'));
+      
+  fetch(API_URL+"/save", {
+    method: "POST",
+    body: title,
+
+  })
+  .then(function(response) {
+    if(response.ok){
+    return response.json();
+  }else{
+    throw "error en la llamada";
+  }
+})
+  .then(function(data) {
+   console(texto);
+  })
+  .catch(function(error) {
+    console.log(error);
+  });
+  
+});
+
+
+/*
+close_button.addEventListener("click", function(e) {
+    e.preventDefault();
+    document.getElementById("window-notice").style.display = "none";
+});
+
+*/
 
 
